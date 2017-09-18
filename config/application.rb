307f2c8ab871b57b0fef8593bb5fc1ba1aac76a3
John Schoeman <johnschoeman1617@gmail.com>
2017-09-18 11:53:14 -0700

    Add jbuild key format to application.rb

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module ProductHuntApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    Jbuilder.key_format camelize: :lower
   end
 end
