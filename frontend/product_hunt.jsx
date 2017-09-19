2a89c6bd63567bc92ef82813785a422b190432c5
John Schoeman <johnschoeman1617@gmail.com>
2017-09-19 12:08:03 -0700

    Initialize react/redux and dependencies

---
+++ frontend/product_hunt.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+    let root = document.getElementById("root");
+    
+    ReactDOM.render(<h1>Product Hunt App</h1>, root);
+});
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/product_hunt.jsx",
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
+};
