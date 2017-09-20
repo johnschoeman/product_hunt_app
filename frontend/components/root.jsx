f146f357e555b7e19056e1f056d4df694f82b9ca
John Schoeman <johnschoeman1617@gmail.com>
2017-09-19 21:56:21 -0700

    Add semicolons

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
-import App from './app'
+import App from './app';
 
 const Root = ({store}) => (
     <Provider store={ store } >
@@ -9,6 +9,6 @@ const Root = ({store}) => (
             <App />
         </HashRouter>
     </Provider>
-)
+);
 
 export default Root;
