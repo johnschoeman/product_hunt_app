bf508f0fb20c478cad3b49ee92cf81a96104052d
John Schoeman <johnschoeman1617@gmail.com>
2017-09-19 21:51:58 -0700

    remove redex-devtools-extenstion from store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 
 const configureStore = (preloadedState = {}) => (
     createStore(
