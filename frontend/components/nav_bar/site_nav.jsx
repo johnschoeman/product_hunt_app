8bac6e51e25e296e9215f253ba8f47c55b35862c
John Schoeman <johnschoeman1617@gmail.com>
2017-09-20 13:25:00 -0700

    Add heroku link to readme.md

--- frontend/components/nav_bar/site_nav.jsx
+++ frontend/components/nav_bar/site_nav.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export default () => (
-  <div>
-    site nav
-  </div>
-);
+class SiteNav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.showMetaMenu = this.showMetaMenu.bind(this);
+  }
+
+  showMetaMenu() {
+    console.log('clicked Meta Menu');
+  }
+
+  render() {
+    return (
+      <hgroup className="header-group site-nav">
+        <Link to="/ship">Ship</Link>
+        <Link to="/ask">Ask</Link>
+        <Link to="/jobs">Jobs</Link>
+        <button className="meta-menu-button" onClick={this.showMetaMenu} >...</button>
+      </hgroup>
+    );
+  }
+}
+
+export default SiteNav;
--- frontend/components/nav_bar/user_nav.jsx
+++ frontend/components/nav_bar/user_nav.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
 
 class UserNav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addProduct = this.addProduct.bind(this);
+    this.showNotificaitons = this.showNotificaitons.bind(this);
+    this.showUserMenu = this.showUserMenu.bind(this);
+  }
+
+  addProduct() {
+    console.log('clicked Add Product');
+  }
+
+  showNotificaitons() {
+    console.log('cliked Show Notifications');
+  }
+
+  showUserMenu() {
+    console.log('clicked Show User Menu');
+  }
     
   render()  {
     let currentUser = this.props.currentUser;
     let logout = this.props.logout;
     
     return ( 
-      <hgroup className="header-group">
-        <h2 className="header-name">Hi, {currentUser.username}!</h2>
-        <button className="header-button" onClick={logout}>Log Out</button>
+      <hgroup className="header-group user-nav">
+        <button className="add-product-button" onClick={this.addProduct}>+</button>
+        <button className="notification-seed-button" onClick={this.showNotificaitons}>notifications</button>
+        <button className="user-menu-button" onClick={logout}>User Menu</button>
       </hgroup> 
       );
     }
