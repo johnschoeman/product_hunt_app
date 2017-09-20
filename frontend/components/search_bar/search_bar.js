afd7a247080fc440d2e988ab0cb4e10bc966e627
John Schoeman <johnschoeman1617@gmail.com>
2017-09-20 13:33:59 -0700

    Update nav bar buttons search and links

--- frontend/components/search_bar/search_bar.js
+++ frontend/components/search_bar/search_bar.js
@@ -3,12 +3,22 @@ import React from 'react';
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
+    this.update = this.update.bind(this);
+    this.state = { searchTerm: "" };
+  }
+
+  update(e) {
+    this.setState({searchTerm: e.target.value});
   }
 
   render() {
     return (
       <div>
-        search bar component
+        <input type="search" 
+               value={this.state.searchTerm}
+               onChange={this.update}
+               placeholder="Discover your next favorite thing">
+        </input>
       </div>
       );
   }
