03d47767c93edafc542cc976b9a22596b7cda192
John Schoeman <johnschoeman1617@gmail.com>
2017-09-22 15:25:06 -0700

    Change product new route to show

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :show, :index]
     resource :session, only: [:create, :new, :destroy]
-    resources :products, only: [:create, :new, :index]
+    resources :products, only: [:index, :create, :show]
   end
 
 
