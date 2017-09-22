f4e37a0c2e5ca98b908ca22c29cf3e2f6972162d
John Schoeman <johnschoeman1617@gmail.com>
2017-09-22 10:42:58 -0700

    Add cloudinary to gemfile

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'jquery-rails'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'cloudinary'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- frontend/components/dropdown/dropdown.jsx
+++ frontend/components/dropdown/dropdown.jsx
@@ -27,7 +27,7 @@ class Dropdown extends React.Component {
     return (
       <div className="dropdown">
         <div className="user-profile-image" onClick={this.showDropdown}> hi </div>
-        <div className="dropdown-content" >
+        <div className="dropdown-content hide" >
           <ul>
             <li className="dropdown-item" onClick={this.handleRedirect(`/users/${currentUser.id}`)}>Profile</li>
             <li className="dropdown-item" onClick={this.handleRedirect('/products/new')}>Hunt Product</li>
