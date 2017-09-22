c909bcf1be2b22f0d5dfc7155d361f89f680e8ef
John Schoeman <johnschoeman1617@gmail.com>
2017-09-22 11:34:27 -0700

    Stub out user-profile-img

--- frontend/components/dropdown/dropdown.jsx
+++ frontend/components/dropdown/dropdown.jsx
@@ -26,7 +26,7 @@ class Dropdown extends React.Component {
     let currentUser = this.props.currentUser;
     return (
       <div className="dropdown">
-        <div className="user-profile-image" onClick={this.showDropdown}> hi </div>
+        <div className="user-profile-img" onClick={this.showDropdown}></div>
         <div className="dropdown-content hide" >
           <ul>
             <li className="dropdown-item" onClick={this.handleRedirect(`/users/${currentUser.id}`)}>Profile</li>
