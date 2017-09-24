eff6998e1962e83dffbdfb701cb7a7a4e51c6d97
John Schoeman <johnschoeman1617@gmail.com>
2017-09-23 21:04:14 -0700

    Get dropdown to transition smoothly

--- frontend/components/dropdown/dropdown.jsx
+++ frontend/components/dropdown/dropdown.jsx
@@ -28,13 +28,13 @@ class Dropdown extends React.Component {
       <div className="dropdown">
         <div className="user-profile-img-container" onClick={this.showDropdown}>
           <img className="user-profile-img" src="https://res.cloudinary.com/dekgrddbo/image/upload/v1506105323/ray_charles_headshot_vljkuq.jpg"/>
-        </div>
         <div className="dropdown-content hide" >
           <ul>
             <li className="dropdown-item" onClick={this.handleRedirect(`/users/${currentUser.id}`)}>Profile</li>
             <li className="dropdown-item" onClick={this.handleRedirect('/products/new')}>Hunt Product</li>
             <li className="dropdown-item" onClick={this.props.logout}>Log Out</li>
           </ul>
+        </div>
         </div>
       </div>
     );
