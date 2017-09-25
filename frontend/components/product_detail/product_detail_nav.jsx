7bb1a14b2f90368985c8571f1ee8621628a54f9d
John Schoeman <johnschoeman1617@gmail.com>
2017-09-25 13:48:06 -0700

    Modify website link on product detail

--- frontend/components/product_detail/product_detail_nav.jsx
+++ frontend/components/product_detail/product_detail_nav.jsx
@@ -4,15 +4,21 @@ import { Link } from 'react-router-dom';
 class ProductDetailNav extends React.Component {
 
   render() {
-    
+    let product = this.props.product;
+    let linkUrl = product.linkUrl;
+    if (!product.linkUrl) {
+      linkUrl = "/";
+    }
     return (
       <div className="product-detail-nav">
         <div className="product-detail-nav-upvote-container">
           <button className="orange-button large-size" value="Upvote">Upvote</button>
         </div>
         <div className="product-detail-nav-link-container">
-          <Link to="/" className="button white-button large-size" >Webstie</Link>
-          <Link to="/" className="button white-button large-size" >Chrome Store</Link>
+          <a href={linkUrl} className="button white-button large-size">
+            <div>Website</div>
+            <div>{linkUrl}</div>
+          </a>
         </div>
       </div>
     );
