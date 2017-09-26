9f9e769508843f27d5782311a88fe4603127504a
John Schoeman <johnschoeman1617@gmail.com>
2017-09-26 16:51:50 -0700

    Add auto resize to new comment form

--- frontend/components/product_discussion/product_discussion_comment_form.jsx
+++ frontend/components/product_discussion/product_discussion_comment_form.jsx
@@ -8,30 +8,50 @@ class CommentForm extends React.Component {
       commentText: ""
     };
 
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.autoSize = this.autoSize.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('submit with: ', this.state.commentText);
   }
 
   handleChange(e) {
     this.setState({commentText: e.target.value});
   }
 
+  autoSize() {
+    let textArea = document.getElementById('textArea');
+    let text = textArea.value.replace(/\n/g, '<br/>') + '&nbsp;';
+    let textCopy = document.getElementById('textCopy');
+    textCopy.innerHTML = text;
+  }
+
   render() {
     let user = this.props.user;
     return (
-      <div className="new-comment-form">
+      <form className="new-comment-form" onSubmit={this.handleSubmit}>
         <div className="new-comment-form-input-bar">
           <div className="user-profile-img-container">
             <img className="user-profile-img" src={user.imageUrl}/>
           </div>
-          <textarea type="text" 
-                placeholder="What do you think of this product..." 
-                onChange={this.handleChange}
-                value={this.state.commentText}/>
+          <div id="textContainer" className="textarea-container">
+            <textarea 
+                  id="textArea"
+                  type="text" 
+                  placeholder="What do you think of this product..." 
+                  onChange={this.handleChange}
+                  onKeyDown={this.autoSize}
+                  value={this.state.commentText}/>
+            <div id="textCopy" className="text-copy"/>
+          </div>
         </div>
         <div className="new-comment-form-button-container">
           <input type="submit" className="button orange-button medium-size" value="Comment"></input>
         </div>
-      </div>
+      </form>
     );
   }
 }
