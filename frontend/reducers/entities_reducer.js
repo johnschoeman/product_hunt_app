38a9e11e3fdb7ca402efcb7a1c88159fed0d7684
John Schoeman <johnschoeman1617@gmail.com>
2017-09-28 16:50:30 -0700

    Remove upvote slice of state

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,12 +3,10 @@ import productsReducer from './products_reducer';
 import viewedUserReducer from './viewed_user_reducer';
 import commentsReducer from './comments_reducer';
 import searchResultsReducer from './search_results_reducer';
-import upvotesReducer from './upvotes_reducer';
 
 export default combineReducers({
   products: productsReducer,
   viewedUser: viewedUserReducer,
   comments: commentsReducer,
-  searchResults: searchResultsReducer,
-  upvotes: upvotesReducer
+  searchResults: searchResultsReducer
 });
