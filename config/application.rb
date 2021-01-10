251d7ec8a1915ff5894bb8ab1de7698bbae76350
John Schoeman <johnschoeman1617@gmail.com>
2021-01-10 09:51:34 -0800

    Update rails 5.1 to 5.2

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,8 @@ module ProductHuntApp
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    Jbuilder.key_format camelize: :lower
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
- # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'static_pages#root'
 
@@ -10,9 +10,6 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :update, :destroy]
     resources :upvotes, only: [:index, :show, :create]
     delete '/upvotes', to: 'upvotes#destroy'
-
     get '/omnisearch', to: 'search#omnisearch'
   end
-
-
 end
