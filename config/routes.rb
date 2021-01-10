e87e99872be0fce72664cb589fa5839874d46902
John Schoeman <johnschoeman1617@gmail.com>
2021-01-10 09:51:34 -0800

    Update rails 5.1 to 5.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
- # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root to: 'static_pages#root'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:index, :show, :create, :update]
@@ -10,9 +8,6 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :update, :destroy]
     resources :upvotes, only: [:index, :show, :create]
     delete '/upvotes', to: 'upvotes#destroy'
-
     get '/omnisearch', to: 'search#omnisearch'
   end
-
-
 end
