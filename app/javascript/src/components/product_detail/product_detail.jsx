965e0948b171311e78515b83bf8b692a884c7c5a
John Schoeman <johnschoeman1617@gmail.com>
2021-01-10 17:46:11 -0800

    fix product detail page

--- app/javascript/src/components/product_detail/product_detail.jsx
+++ app/javascript/src/components/product_detail/product_detail.jsx
@@ -1,48 +1,45 @@
-import React from 'react';
+import React, { useEffect } from "react"
 
-import ProductDetailHeader from './product_detail_header';
-import ProductDetailMedia from './product_detail_media';
-import ProductDiscussionContainer from '../product_discussion/product_discussion_container';
-import ProductDetailNav from './product_detail_nav';
+import ProductDetailHeader from "./product_detail_header"
+import ProductDiscussionContainer from "../product_discussion/product_discussion_container"
+import ProductDetailNav from "./product_detail_nav"
 
-class ProductDetail extends React.Component {
-  constructor(props) {
-    super(props);
+const ProductDetail = ({
+  product,
+  viewedProductId,
+  fetchProduct,
+  createUpvote,
+  destroyUpvote,
+}) => {
+  useEffect(() => {
+    fetchProduct(viewedProductId)
+  }, [])
 
-  }
-
-  componentDidMount() {
-    this.props.fetchProduct(this.props.viewedProductId);
-  }
-
-  render() {
-    let product = this.props.product;
-    let createUpvote = this.props.createUpvote;
-    let destroyUpvote = this.props.destroyUpvote;
-
-    return (
-      <div className="product-detail-container">
-          <ProductDetailHeader product={product}/>
-        <div className="product-detail-main-content-container">
-          <div className="product-detail-content-bar">
-            <div className="product-detail-content-header">
-              <h3>DISCUSSION</h3>
-            </div>
-            <ProductDiscussionContainer 
-                  product={product} 
-                  viewedProductId={this.props.viewedProductId}
-                  fetchProduct={this.props.fetchProduct} />
+  return (
+    <div className="product-detail-container">
+      <ProductDetailHeader product={product} />
+      <div className="product-detail-main-content-container">
+        <div className="product-detail-content-bar">
+          <div className="product-detail-content-header">
+            <h3>DISCUSSION</h3>
           </div>
-          <div className="product-detail-aside">
-            <ProductDetailNav product={product}
-                              createUpvote={createUpvote}
-                              destroyUpvote={destroyUpvote} />
-          </div>
+          <ProductDiscussionContainer
+            product={product}
+            viewedProductId={viewedProductId}
+            fetchProduct={fetchProduct}
+          />
+        </div>
+        <div className="product-detail-aside">
+          <ProductDetailNav
+            product={product}
+            createUpvote={createUpvote}
+            destroyUpvote={destroyUpvote}
+          />
         </div>
       </div>
-    );
-  }
+    </div>
+  )
 }
 
-{/* <ProductDetailMedia /> */}
-export default ProductDetail;
+export default ProductDetail
+
--- app/javascript/src/components/product_detail/product_detail_container.js
+++ app/javascript/src/components/product_detail/product_detail_container.js
@@ -1,36 +1,32 @@
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router';
-import ProductDetail from './product_detail';
-import { fetchProduct } from '../../actions/product_actions';
-import { setViewedProduct } from '../../actions/ui_actions';
-import { fetchProductDetailUpvotes, createUpvote, destroyUpvote } from '../../actions/upvote_actions';
+import { connect } from "react-redux"
+import { withRouter } from "react-router"
+import ProductDetail from "./product_detail"
+import { fetchProduct } from "../../actions/product_actions"
+import { createUpvote, destroyUpvote } from "../../actions/upvote_actions"
 
 const mapStateToProps = (state, ownProps) => {
-  const viewedProductId = ownProps.match.params.productId;
-  let product = state.entities.products.byId[viewedProductId];
-  let currentUser = state.session.currentUser;
+  const viewedProductId = ownProps.match.params.productId
+  let product = state.entities.products.by_id[viewedProductId]
   if (product === undefined) {
-    product = {};
+    product = {}
   }
   return {
     product,
     viewedProductId,
-    currentUser
-  };
-
-
-
-};
+  }
+}
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-
   return {
-    fetchProduct: (productId) => (dispatch(fetchProduct(productId))),
-    setViewedProduct: (productId) => (dispatch(setViewedProduct(productId))),
-    fetchProductDetailUpvotes: (userId, upvoteableType) => dispatch(fetchProductDetailUpvotes(userId, upvoteableType)),
-    createUpvote: (upvoteableType, upvoteableId) => dispatch(createUpvote(upvoteableType, upvoteableId)),
-    destroyUpvote: (upvoteableType, upvoteableId) => dispatch(destroyUpvote(upvoteableType, upvoteableId))
-  };
-};
+    fetchProduct: (productId) => dispatch(fetchProduct(productId)),
+    createUpvote: (upvoteableType, upvoteableId) =>
+      dispatch(createUpvote(upvoteableType, upvoteableId)),
+    destroyUpvote: (upvoteableType, upvoteableId) =>
+      dispatch(destroyUpvote(upvoteableType, upvoteableId)),
+  }
+}
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(ProductDetail));
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(ProductDetail)
+)
+
